fd75dd22e56be96320255a6534bba41131765856
YangTh599 <YangTh599@northwested.org>
2025-03-11 11:28:00 -0400

    updated classes
    New shape parent class:
    updated all necessary shapes to have Shape as a parent class
    
    updated textboxes classes to update text

--- pygame_template.py
+++ pygame_template.py
@@ -50,10 +50,6 @@ def main(): # MAIN FUNCTION
         clock.tick(FPS) # FPS Tick
 
         run = handle_events()
-        # for event in pygame.event.get():
-        #     if event.type == pygame.QUIT: # QUIT
-        #         run = False
-        #         break
         
 
         
